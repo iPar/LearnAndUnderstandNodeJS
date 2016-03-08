5ef8515857f26424d116ec01045dc0771b06436f
iPar <parha100@mail.chapman.edu>
2016-03-08 11:09:41 -0800

    Added views/ directory and different views to the repo.  Also set the view engine to "ejs" after including ejs using npm install ejs -save.

--- Express/app.js
+++ Express/app.js
@@ -11,17 +11,19 @@ var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
+app.set('view engine', 'ejs');
+
 app.use('/', function(req, res, next) {
     console.log('Request Url: ' + req.url);
     next();
 });
 
 app.get('/', function(req, res) {
-    res.send('<html><head><link href="assets/style.css" type="text/css" rel="stylesheet"/></head><body><h1>Hello world!</h1></body></html>');
+    res.render('index');            //  Using the view engine defined above to render index.ejs from the default 'views' folder.
 });
 
 app.get('/person/:id', function(req, res) {
-    res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
+    res.render('person', { ID: req.params.id });  //  Passing in object as variable to 'person.ejs' template.
 });
 
 app.get('/api', function(req, res) {
