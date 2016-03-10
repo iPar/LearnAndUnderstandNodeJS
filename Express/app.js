74fde723a3acc14585a866cd561bc93176a31b27
iPar <parha100@mail.chapman.edu>
2016-03-10 15:17:42 -0800

    Latest changes to app.js in Express/, which now connects to mlab mongodb, creating new Person objects and outputting all Person objects to the console.

--- Express/app.js
+++ Express/app.js
@@ -8,8 +8,46 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var dbKey = require('./dbKey');
+var uniqKey = dbKey();
 
-mongoose.connect(dbKey);
+console.log(uniqKey);
+mongoose.connect(uniqKey);
+
+var Schema = mongoose.Schema;
+
+var personSchema = new Schema({
+    firstname: String,
+    lastname: String,
+    address: String
+});
+
+var Person = mongoose.model('Person', personSchema);
+
+var john = Person({
+    firstname: 'John',
+    lastname: 'Doe',
+    address: '1234 Temple Drive'
+});
+
+//  save the user
+john.save(function(err) {
+    if (err) throw err;
+
+    console.log('person saved!');
+});
+
+var jane = Person({
+    firstname: 'jane',
+    lastname: 'Doe',
+    address: '1234 Temple Drive'
+});
+
+//  save the user
+jane.save(function(err) {
+    if (err) throw err;
+
+    console.log('person saved!');
+});
 
 var apiController = require('./controllers/apiController');
 var htmlController = require('./controllers/htmlController');
@@ -22,6 +60,15 @@ app.set('view engine', 'ejs');
 
 app.use('/', function(req, res, next) {
     console.log('Request Url: ' + req.url);
+
+    //  get all the users
+    Person.find({}, function(err, users) {
+        if (err) throw err;
+
+        //  object of all users
+        console.log(users);
+    });
+
     next();
 });
 
