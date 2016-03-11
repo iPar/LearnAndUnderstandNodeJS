99c78734b1e453e2a7a28166f34e611db5351279
iPar <parha100@mail.chapman.edu>
2016-03-10 17:47:22 -0800

    Added client side rendering of html template and passing of JSON blob to client side app.  Person data passed to client to be rendered in the DOM by Angular.

--- MeanApp/app.js
+++ MeanApp/app.js
@@ -6,11 +6,23 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+var people = [
+    {
+        name: 'John Doe'
+    },
+    {
+        name: 'Jane Doe'
+    },
+    {
+        name: 'Jim Doe'
+    }
+];
+
 app.set('view engine', 'ejs');
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index', { serverPeople: people });
 });
 
 app.listen(port);
